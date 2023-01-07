105a84b18e5a758c12d9c844e35ff0c58e38c7b1
TK-Elliot <74335372+TK-Elliot@users.noreply.github.com>
2023-01-06 19:01:15 -0800

    Add files via upload

---
+++ keyboard_converter.py
@@ -0,0 +1,15 @@
+# This is a script for convert Japanese keyboard to US keyboard.
+
+convert_dict = {"\"": "@", "&": "^", "'": "&", "(": "*", ")": "(", "=": "_", 
+        "~": "+", "^": "=", "|": "}", "\\": "/", "`": "{", "@": "]", "{": "}", "[": "]",
+        "+": ":", "*": "\"", ":": "'", "}": "|", "]": "\\"}
+string = input("Type your string: ")
+replaced = []
+for s in string:
+  try:
+    s = convert_dict[s]
+  except:
+    s = s 
+  replaced.append(s)
+result = ''.join(map(str, replaced))
+print(result)
